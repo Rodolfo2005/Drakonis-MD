f82ef76d49c7cac7dfc887461ec0f1a53569b112
Rodolfo2005 <charlesrodolfo06@gmail.com>
2025-07-27 01:22:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~UrVjEaTK#2LyiT2h1zlqOPvMlowU4LPgeZGRhUZ_G5Dkn6Ouf8eY ",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  PREFIX: "," // prefix (e.g., ., /, !, *)
 };
